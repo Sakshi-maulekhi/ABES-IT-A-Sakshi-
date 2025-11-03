697b4d5228dc22125df56661ce2de286adb6b7c2
Sakshi Maulekhi <sakshimaulekhi84@gmail.com>
2025-11-03 13:01:45 +0530

    book-app

---
+++ BookStore/book-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Book from './components/book.jsx'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Book/>
+      <br/>
+      <Book/>
+      <br/>
+      <Book/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ BookStore/book-app/src/components/book.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './book.css'
+function Book() {
+  return (
+    <div id='book'>
+        <img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Forange-publishers.com%2Fnonfiction-book-writing%2F&psig=AOvVaw3ZCX5KI3N6QxW_cV2sy17n&ust=1762240852698000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMD7iva41ZADFQAAAAAdAAAAABAE" alt = ""/>
+        <h1>Title:Math</h1>
+        <h1>250</h1>
+        <div>
+            <button>+</button>
+            <span>0</span>
+            <button>-</button>
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default Book
